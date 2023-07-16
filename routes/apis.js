3ae2ca3916eea348edd0b425ec7e75e2c4a3bf27
macOS <macos@macOSs-MacBook-Pro-3.local>
2023-07-16 21:13:44 +0700

    first commit

---
+++ routes/apis.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var router = express.Router();
+let Country = require('country-state-city').Country;
+let State = require('country-state-city').State;
+let City = require('country-state-city').City;
+
+router.get('/countries', function(req, res, next) {
+  try {
+    res.json(Country.getAllCountries()) 
+  } catch (error) {
+    res.json([])
+  }
+});
+
+router.get('/countries/:code', function(req, res, next) {
+  try {
+    res.json(Country.getCountryByCode(req.params.code))
+  } catch (error) {
+    res.json([])
+  }
+});
+
+router.get('/countries/:code/states', function(req, res, next) {
+  try {
+    res.json(State.getStatesOfCountry(req.params.code))
+  } catch (error) {
+    res.json([])
+  }
+});
+
+router.get('/countries/:code/cities', function(req, res, next) {
+  try {
+    res.json(City.getCitiesOfCountry(req.params.code))
+  } catch (error) {
+    res.json([])
+  }
+});
+
+router.get('/states/:code/cities', function(req, res, next) {
+  try {
+    res.json(City.getCitiesOfState(req.params.code))
+  } catch (error) {
+    res.json([])
+  }
+});
+
+
+
+module.exports = router;
